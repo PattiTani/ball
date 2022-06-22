ec1af73e17c5ca6d16cd200d246d8ff3a10b14cc
PattiTani <patricia.tani@gmail.com>
2022-06-22 10:56:43 -0700

    original ball4

--- ball4.cc
+++ ball4.cc
@@ -2,55 +2,40 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/System.hpp>
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
-#include <string>
+
 using namespace sf;
 
-class Ufo {
+class Ball {
 
     public:
-        Ufo(const RenderWindow& W, int rad, int x, int y, int r, int g, int b);
-        CircleShape C;    //must be public so we can draw it    
-        void move(float x, float y);
-        void update();
-        Sprite sprite;
-        Vector2f pos;
-        float rad;
-        float winwidth; 
-		float winheight ;
-        
-    private:
-        sf::Texture texture;
-		
-};
 
-class Meteor {
-    public:
-		Meteor(const RenderWindow& W, int rad, int x, int y, int r, int g, int b);
+		Ball(const RenderWindow& W, int rad, int x, int y, int r, int g, int b);
 		void move(float x, float y);
 		void update();
-        int randx, randy;
 		Sprite sprite; //must be public so we can draw it	
 	
 	private:
 		RenderWindow* W;
-		Vector2f pos;	
+		Vector2f pos;		   
 		float rad;
 		float winwidth; //assigned in ctor method below
 		float winheight ;
 		//float winwidth( window.getSize().x); //fails as no window object in Ball class
 		//float winheight( window.getSize().y);
 		Texture texture; //static so we don't have a texture for each instance
+		//bool collided;
+		
+		
 };
+
 
 int main()
 {
     //Ball ball(50, 100, 30, 250, 0, 0);
+
 	Event event;
     RenderWindow window(VideoMode(800,600), "SFML Test");
-    Ufo ufo(window, 50, 100, 30, 250, 0, 0);
-    Meteor ball(window, 50, 100, 30, 250, 0, 0);
+    Ball ball(window, 50, 100, 30, 250, 0, 0);
     while(window.isOpen())
     {
         while(window.pollEvent(event))
@@ -67,13 +52,10 @@ int main()
 
         }
 
-        ufo.update();
-        ball.move(8,20);
         ball.update();
 		window.setFramerateLimit(60);
         window.clear(Color::White);
         window.draw(ball.sprite);
-        window.draw(ufo.sprite);
         window.display();
     }
     return 0;
@@ -81,12 +63,10 @@ int main()
 }
 
 
-Meteor::Meteor(const RenderWindow& W, int rad, int x, int y, int r, int g, int b)
+Ball::Ball(const RenderWindow& W, int rad, int x, int y, int r, int g, int b)
 {
-    srand(time(0));
-    randx = (rand()%20)-10;
-    randy = (rand()%20)-10;
-    std::cout<<randx;
+	//W.setTitle("aoiresntoiaen"); //fails cause W is passed as const ref
+	//W.setSize(Vector2u(500.,500.));
 	winheight= W.getSize().y;
 	winwidth= W.getSize().x;
     //texture.loadFromFile("meteor-small.png");
@@ -94,22 +74,7 @@ Meteor::Meteor(const RenderWindow& W, int rad, int x, int y, int r, int g, int b
     Vector2u v= texture.getSize();
     float xs= 100./v.x; //make sprite 100 pixels wide and high
     float ys= 100./v.y;
-    sprite.setTexture(texture);
-    sprite.setOrigin(v.x/2,v.y/2); 
-    // must setOrigin to center *before* scaling 
-    sprite.setScale(xs,ys);
-	sprite.setPosition(x,y);
-	//collided = false;
-}
-
-Ufo::Ufo(const RenderWindow& W, int rad, int x, int y, int r, int g, int b)
-{
-	winheight= W.getSize().y;
-	winwidth= W.getSize().x;
-    texture.loadFromFile("ufo.png");
-    Vector2u v= texture.getSize();
-    float xs= 100./v.x; //make sprite 100 pixels wide and high
-    float ys= 100./v.y;
+    std::cout<<xs<<std::endl;
     sprite.setTexture(texture);
     sprite.setOrigin(v.x/2,v.y/2); 
     // must setOrigin to center *before* scaling 
@@ -121,18 +86,13 @@ Ufo::Ufo(const RenderWindow& W, int rad, int x, int y, int r, int g, int b)
 }
 
 
-void Ufo::move(float x, float y)
+void Ball::move(float x, float y)
 {
 	sprite.move(x,y);
 }
 
-void Meteor::move(float x, float y)
-{
-	sprite.move(x,y);
-}
-
-
-void Ufo::update(){
+void Ball::update(){
+	
 	if(Keyboard::isKeyPressed(Keyboard::Left))
 	{
 		sprite.move(-10,0.);
@@ -149,8 +109,10 @@ void Ufo::update(){
 	{
 		sprite.move(0,-10);
 	}
+	
 	pos = sprite.getPosition();
 	float rad = 50.;
+		
 	if ( pos.x - rad > winwidth) //beyond right side
     {   
         pos.x = 0;
@@ -167,29 +129,7 @@ void Ufo::update(){
     if( pos.y + rad < 0) //beyond top
     {   
         pos.y = winheight;
-    }
+    }   
 	sprite.setPosition(pos.x, pos.y);
-}
-
-void Meteor::update(){
-	pos = sprite.getPosition();
-	if ( pos.x - rad > winwidth) //beyond right side
-    {   
-        pos.x = 0;
-    }   
-    if ( pos.y - rad > winheight) //beyond bottom
-    {   
-        pos.y = 0;
-    }   
-    
-    if( pos.x + rad < 0) //beyond left side
-    {   
-        pos.x = winwidth;
-    }   
-    if( pos.y + rad < 0) //beyond top
-    {   
-        pos.y = winheight;
-    }
-    sprite.rotate(4);
-	sprite.setPosition(pos.x, pos.y);
+	sprite.rotate(4);
 }
